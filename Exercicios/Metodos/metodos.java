f938583815fa2665b8439bc47ff2a16bf4d4e775
MADOXX7 <unknowguy736@gmail.com>
2026-09-04 10:05:44 -0300

    Adicionar classe 'metodos' com método de exemplo e método principal

---
+++ Exercicios/Metodos/metodos.java
@@ -0,0 +1,17 @@
+package Exercicios.Metodos;
+
+public class metodos { // Declaração de classe
+
+    public void EscreverMensagem() { // EscreverMensagem é um método de exemplo
+        System.out.println("Olá mundo");
+    }
+    
+    public static void main(String[] args) { // Método principal 
+
+        metodos obj = new metodos(); // Cria instância da classe contendo o metodo 
+
+        obj.EscreverMensagem(); // chamando o metodo
+        
+    }
+
+}
