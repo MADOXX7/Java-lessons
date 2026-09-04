addf83ab1f707da2585c288cc82777193a01225d
MADOXX7 <unknowguy736@gmail.com>
2026-09-04 10:06:19 -0300

    Remover classe 'metodos' e arquivo 'notas.md'; adicionar arquivo 'notes.txt' com tipos primitivos e nova classe 'variaveis' com exemplos de declaração e inicialização de variáveis.

---
+++ Exercicios/Variaveis/variaveis.java
@@ -0,0 +1,24 @@
+package Exercicios.Variaveis;
+
+public class variaveis {
+public static void main(String[] args) {
+     //Declaração de variável
+    int idade; // tipo + nome
+    //inicialização da variável
+    idade = 18; // variavel + "=" + valor
+
+    //Declaração com inicialização 
+    int altura = 185;
+
+    // Todos os tipos primitivos
+    byte bytee = -128; 
+    short shortt = 9999;
+    int inteiro = 999999999;
+    long longos = 999999999;
+    float flutuante = 9.9F;
+    double flutuanteMaior = 9.9D;
+    char caractere = 'A';
+    boolean verdadeiroFalso = true;
+
+}
+}
